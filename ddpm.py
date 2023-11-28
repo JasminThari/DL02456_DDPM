8d4b4fecf410095c4db93eea548b36431721c258
JasminThari <s204155@student.dtu.dk>
2023-11-28 15:05:09 +0100

    h

--- ddpm.py
+++ ddpm.py
@@ -1,6 +1,7 @@
 
-
 import os
+
+import numpy as np
 import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt
@@ -8,53 +9,54 @@ from tqdm import tqdm
 from torch import optim
 from utils import *
 from modules import UNet
-import logging
 from torch.utils.tensorboard import SummaryWriter
+import argparse
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+class DiffusionProcess:
+    def __init__(self, img_shape = (3, 64, 64), T = 100, beta_min = 10e-4, beta_max=20e-3, device="cuda"):
+        self.img_shape = img_shape
+        self.T = T
+        self.beta_min = beta_min
+        self.beta_max = beta_max
+        self.beta = torch.linspace(self.beta_min, self.beta_max, self.T)
+        self.alpha = 1-self.beta
+        self.alpha_bar = torch.cumprod(self.alpha, 0)
+        self.device=device
 
+    def noising(self, x0, t):
+        """
+        This function implements equation (4) in the paper. This will respond to forward process of the DDPM model.
+        :param x0: Image without any noise
+        :param t: timestep
+        :return: noisy image, noise
+        """
+        noise = torch.randn(x0.shape).to(self.device)
+        mu = torch.sqrt(self.alpha_bar[t]).to(self.device)
+        #var = torch.full_like(mu, 1-self.alpha_bar[t])
+        var = torch.full_like(mu, 1 - self.alpha_bar[t].item()).to(self.device)
+        noisy_image = mu * x0 + torch.sqrt(var) * noise
+        return noisy_image, noise
 
-class Diffusion:
-    def __init__(self, noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=256, device="cuda"):
-        self.noise_steps = noise_steps
-        self.beta_start = beta_start
-        self.beta_end = beta_end
-        self.img_size = img_size
-        self.device = device
-
-        self.beta = self.prepare_noise_schedule().to(device)
-        self.alpha = 1. - self.beta
-        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
-
-    def prepare_noise_schedule(self):
-        return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
-
-    def noise_images(self, x, t):
-        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
-        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
-        Ɛ = torch.randn_like(x)
-        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ
-
-    def sample_timesteps(self, n):
-        return torch.randint(low=1, high=self.noise_steps, size=(n,))
-
-    def sample(self, model, n):
-        logging.info(f"Sampling {n} new images....")
+    def sampling(self, model, num_img, variance_type=None):
+        x = torch.randn((num_img, self.img_shape[0], self.img_shape[1], self.img_shape[2]),device=self.device)
         model.eval()
+
         with torch.no_grad():
-            x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
-            # x = torch.zeros((n,3, self.img_size, self.img_size)).to(self.device)
-            for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
-                t = (torch.ones(n) * i).long().to(self.device)
-                predicted_noise = model(x, t)
-                alpha = self.alpha[t][:, None, None, None]
-                alpha_hat = self.alpha_hat[t][:, None, None, None]
-                beta = self.beta[t][:, None, None, None]
-                if i > 1:
-                    noise = torch.randn_like(x)
+            for iter_t in reversed(range(self.T)):
+                t = (torch.ones(num_img) * iter_t).long().to(self.device)
+                if iter_t > 1:
+                    z = torch.randn_like(x)
                 else:
-                    noise = torch.zeros_like(x)
-                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+                    z = 0
+                    # torch.zeros_like(x)
+                if variance_type is not None:
+                    if variance_type == "Type2":
+                        var = self.beta[iter_t]
+                else:
+                    var = (1 - self.alpha_bar[iter_t - 1]) / (1 - self.alpha_bar[iter_t]) * self.beta[iter_t]
+
+                predicted_noise = model(x, t)
+                x = 1 / torch.sqrt(self.alpha[iter_t]) * (x - ((1 - self.alpha[iter_t]) / (torch.sqrt(1 - self.alpha_bar[iter_t]))) * predicted_noise) + torch.sqrt(var) * z
         model.train()
         x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
@@ -62,64 +64,61 @@ class Diffusion:
 
 
 def train(args):
-    setup_logging(args.run_name)
+
     device = args.device
     dataloader = get_data(args)
     model = UNet().to(device)
     optimizer = optim.AdamW(model.parameters(), lr=args.lr)
     mse = nn.MSELoss()
-    diffusion = Diffusion(img_size=args.image_size, device=device)
-    logger = SummaryWriter(os.path.join("runs", args.run_name))
-    l = len(dataloader)
+    diffusion = DiffusionProcess(vars(args))
 
+    model.train()
     for epoch in range(args.epochs):
-        logging.info(f"Starting epoch {epoch}:")
-        pbar = tqdm(dataloader)
-        for i, (images, _) in enumerate(pbar):
-            images = images.to(device)
-            t = diffusion.sample_timesteps(images.shape[0]).to(device)
-            x_t, noise = diffusion.noise_images(images, t)
-            predicted_noise = model(x_t, t)
+        for img_batch, _ in dataloader:
+            img_batch = img_batch.to(device)
+            random_timestep = torch.randint(low=1, high=args.T, size=(1,)).to(device)
+            noisy_image, noise = diffusion.noising(img_batch, random_timestep)
+            predicted_noise = model(noisy_image, random_timestep).to(device)
             loss = mse(noise, predicted_noise)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            pbar.set_postfix(MSE=loss.item())
-            logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+        print(epoch)
 
-        sampled_images = diffusion.sample(model, n=images.shape[0])
-        save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
-        torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
-
-
-def launch():
-    import argparse
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    args.run_name = "DDPM_Uncondtional"
-    args.epochs = 500
-    args.batch_size = 3
-    args.image_size = 64
-    args.dataset_path = r"landscape_img_folder"
-    args.device = "cuda"
-    args.lr = 3e-4
-    train(args)
+    # sampled_images = diffusion.sample(model, n=images.shape[0])
+    # save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
+    # torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
 
 
 if __name__ == '__main__':
-    # launch()
-    device = "cuda"
-    model = UNet().to(device)
-    ckpt = torch.load("models/DDPM_Uncondtional/ckpt.pt")
-    model.load_state_dict(ckpt)
-    diffusion = Diffusion(img_size=64, device=device)
-    x = diffusion.sample(model, 3)
-    print(x.shape)
-    print(x)
-    plt.figure(figsize=(32, 32))
-    plt.imshow(torch.cat([
-        torch.cat([i for i in x.cpu()], dim=-1),
-    ], dim=-2).permute(1, 2, 0).cpu())
-    plt.show()
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.epochs = 500
+    args.batch_size = 3
+    args.device = "cuda"
+    args.lr = 3e-4
+    # args.img_shape = (1, 28, 28)
+    args.img_shape = (3,64,64)
+    args.image_size = 64 # landscape fix
+    args.dataset_path = "landscape_img_folder"
+    args.T = 100
+
+
+    train(args)
+
+    # model = UNet().to(device)
+
+    # Diffusion = DiffusionProcess()
+
+
+    # x0 = torch.ones(5, 3, 4, 4)
+    # t = 2
+    # noisy_image, noise = Diffusion.noising(x0, t)
+
+    sampled_image = Diffusion.sampling(model,1)
+
+    # plt.imshow(sampled_image)
+
+
